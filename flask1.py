7d298bda408b44eb60f57fb763fa25f9daad1570
H0ovy <m.dorogow@gmail.com>
2023-11-22 18:25:06 +0700

    Small design changes and the addition of placeholders for 3 more tasks

--- flask1.py
+++ flask1.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, request, render_template
 
-app = Flask(__name__,static_folder="static_dir")
+app = Flask(__name__)
 
 @app.route('/')
 def transfer():
@@ -13,7 +13,7 @@ def tasks():
 @app.route('/tasks/task1', methods=['GET', 'POST'])
 def task1():
     if request.method == 'POST': 
-        text1 = request.form.get('textarea1')
+        text1 = request.form.get('textarea')
         with open('task1.py', 'w') as f:
             f.write(text1)
 
@@ -22,10 +22,34 @@ def task1():
 @app.route('/tasks/task2', methods=['GET', 'POST'])
 def task2():
     if request.method == 'POST': 
-        text2 = request.form.get('textarea2')
+        text2 = request.form.get('textarea')
         with open('task2.py', 'w') as f:
             f.write(text2)
     return render_template('task2.html')
+
+@app.route('/tasks/task3', methods=['GET', 'POST'])
+def task3():
+    if request.method == 'POST': 
+        text3 = request.form.get('textarea')
+        with open('task3.py', 'w') as f:
+            f.write(text3)
+    return render_template('task3.html')
+
+@app.route('/tasks/task4', methods=['GET', 'POST'])
+def task4():
+    if request.method == 'POST': 
+        text4 = request.form.get('textarea')
+        with open('task3.py', 'w') as f:
+            f.write(text4)
+    return render_template('task4.html')
+
+@app.route('/tasks/task5', methods=['GET', 'POST'])
+def task5():
+    if request.method == 'POST': 
+        text5 = request.form.get('textarea')
+        with open('task5.py', 'w') as f:
+            f.write(text5)
+    return render_template('task5.html')
 
 @app.route("/profile")
 def profileuser():
