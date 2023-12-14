57cde48a7f8672a05f286dc37c5e40bb9b62488c
Imporiooo <egorgureeff@yandex.ru>
2023-12-14 23:17:13 +0700

    added task3 tests

---
+++ task3/user_code.py
@@ -0,0 +1,18 @@
+def fibanacci_num(num):
+
+    fib1 = 0
+
+    fib2 = 1
+
+    stroka=""
+
+    for _ in range(1, num + 1):
+
+        fib1, fib2 = fib1 + fib2, fib1
+
+        stroka+=str(fib1)
+
+        stroka+=" "
+
+    return stroka
+
