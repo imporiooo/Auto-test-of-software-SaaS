8f460183269351de5d5371dcc3f71700542e64bc
Imporiooo <egorgureeff@yandex.ru>
2023-12-15 21:35:24 +0700

    added task 5 tests

---
+++ task5/user_code.py
@@ -0,0 +1,20 @@
+def min_coin_amount(num):
+
+    count = 0
+    while num >= 25:
+        count += 1
+        num = num - 25
+    while num >= 10:
+        count += 1
+        num = num - 10
+    while num >= 5:
+        count += 1
+        num = num - 5
+    while num >= 1:
+        count += 1
+        num = num - 1
+    return count
+
+
+
+        
