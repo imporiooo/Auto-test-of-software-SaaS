387c51a6b81b6bfa94a0d753766598e82eb2d0c1
Imporiooo <egorgureeff@yandex.ru>
2023-12-15 21:34:30 +0700

    added task 3 tests

---
+++ task4/user_code.py
@@ -0,0 +1,29 @@
+from math import *
+
+def quadratic_equation(a,b,c):
+
+
+
+
+
+    d = b**2 - (4 * a * c)
+
+    if d < 0:
+
+        return "No roots"
+
+    elif d == 0:
+
+        x = -(b / (2 * a))
+
+        return x
+
+    elif d > 0:
+
+        x1 = (-b + sqrt(d)) / (2 * a)
+
+        x2 = (-b - sqrt(d)) / (2 * a)
+
+        return min(x1, x2), max(x1,x2)
+
+        
